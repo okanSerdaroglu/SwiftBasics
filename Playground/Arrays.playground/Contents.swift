f82ad867744b1ef9b8512c1defc92df05c102795
bsrAkdg <okanserdaroglu2@gmail.com>
2020-04-09 18:37:46 +0300

    set and array

--- Playground/Arrays.playground/Contents.swift
+++ Playground/Arrays.playground/Contents.swift
@@ -2,6 +2,8 @@ import UIKit
 
 var str = "Hello, playground"
 
+// Array
+
 var myFavouriteMovies = ["Davaro","Kibar Feyzo","Korkusuz Korkak",5] as [Any]
 
 // as -> casting
@@ -17,3 +19,21 @@ myStringArray.last
 myStringArray.sort() // alphabetically sorts
 
 var myNumberArray = [1,2,3,4,5,6]
+myNumberArray
+
+// Set
+
+// set is unordered collection. There is no order. There is no index here
+
+var mySet : Set = [1,2,3,4,5]
+
+mySet.first
+
+var internetArray = [1,1,2,3,4,1,2,2,5]
+
+var myInternetSet = Set(internetArray)
+
+var mySet1 : Set = [1,2,3]
+var mySet2 : Set = [3,4,5]
+var mySet3 : Set = mySet1.union(mySet2)
+print(mySet3)
