7dcddb70f5eb583e708027fbfddafa4c1be8fac0
bsrAkdg <okanserdaroglu2@gmail.com>
2020-04-09 18:18:10 +0300

    any type arrays

---
+++ Playground/Arrays.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var myFavouriteMovies = ["Davaro","Kibar Feyzo","Korkusuz Korkak",5] as [Any]
+
+// as -> casting
+// any -> any
+
+myFavouriteMovies[0] // can not get methods because of any type
+
+var myStringArray = ["test","test1","test2","test3"]
+
+myStringArray[myStringArray.count - 1]
+myStringArray.last
+
+myStringArray.sort() // alphabetically sorts
+
+var myNumberArray = [1,2,3,4,5,6]
