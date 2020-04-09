7fa36da8ccf4dd4d2004ec509ab44699c4d651c0
bsrAkdg <okanserdaroglu2@gmail.com>
2020-04-09 21:41:07 +0300

    if conditions

---
+++ Playground/IfControl.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// if conditions
+
+var myAge = 42
+
+if (myAge < 30){
+    print("30-")
+} else if (myAge > 30 && myAge < 40) {
+    print("30+")
+} else {
+    print("40+")
+}
+
+var myString = "okan"
+
+if (myString == "okan"){
+    print("yes")
+}
