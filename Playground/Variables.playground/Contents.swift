49c1989c045e2c54640da686cce084639ac6d30c
bsrAkdg <okanserdaroglu2@gmail.com>
2020-04-09 17:31:16 +0300

    variables

---
+++ Playground/Variables.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var myNumber = 5*4
+
+print(myNumber)
+
+// snake case user_name
+// camelCase userName
+
+// String
+
+var userName = "Okan"
+userName.append("S")
+userName.lowercased()
+userName.uppercased()
+
+var userSurname = "Serdaroğlu"
+
+userName = "Büşra"
+
+print(userName)
+
+// integer & double & float
+
+let pi = 3.14 // let means constant. val in kotlin
+
+let userAge = 20.0
+
+var number = 6.0
+
+userAge / number
+
+// boolean
+
+var myBoolen = false
