615802108fdf22f76cbac4a3992cc0ae2b2fefae
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-10 20:39:14 +0300

    constraints

---
+++ Layouts/Layouts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Layouts
+//
+//  Created by Okan Serdaroğlu on 10.04.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
