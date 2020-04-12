6033290b6aeaabc051dc3b0447f9dc6d5766d93a
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-12 19:48:12 +0300

    layouts and images added

---
+++ Catch Kenny/Catch Kenny/SplashViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashViewController.swift
+//  Catch Kenny
+//
+//  Created by Okan Serdaroğlu on 12.04.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
