7c0a709b91794b853536f7b540e4106e756d9ab3
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-12 20:02:19 +0300

    home Page

---
+++ Catch Kenny/Catch Kenny/HomePageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomePageViewController.swift
+//  Catch Kenny
+//
+//  Created by Okan Serdaroğlu on 12.04.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
