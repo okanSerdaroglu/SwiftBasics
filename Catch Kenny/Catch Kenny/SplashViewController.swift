1a4a62745f91ba6edf73e1920abd78b983bdf3e1
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-13 11:35:06 +0300

    timer added in splash

--- Catch Kenny/Catch Kenny/SplashViewController.swift
+++ Catch Kenny/Catch Kenny/SplashViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
+    var timer = Timer ()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        counter = 2
+        // this is a simple timer which works @objc timerFunction every second
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func timerFunction (){
+        counter = counter - 1
+        if (counter == 0){
+            timer.invalidate() // stop Timer
+            performSegue(withIdentifier: "splashToSignUp", sender: nil)
+        }
+        
     }
-    */
-
+    
 }
