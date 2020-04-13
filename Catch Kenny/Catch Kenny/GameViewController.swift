b5672810cb28e82acf18fa0b212edb0825ed4260
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-13 21:14:59 +0300

    random number generator added

--- Catch Kenny/Catch Kenny/GameViewController.swift
+++ Catch Kenny/Catch Kenny/GameViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet weak var imageView0: UIImageView!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
+    @IBOutlet weak var imageView6: UIImageView!
+    @IBOutlet weak var imageView7: UIImageView!
+    @IBOutlet weak var imageView8: UIImageView!
+    @IBOutlet weak var imageView9: UIImageView!
+    @IBOutlet weak var imageView10: UIImageView!
+    @IBOutlet weak var imageView11: UIImageView!
+    @IBOutlet weak var imageView12: UIImageView!
+    @IBOutlet weak var imageView13: UIImageView!
+    @IBOutlet weak var imageView14: UIImageView!
 
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var labelTimer: UILabel!
+    var ImageViewArray = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ImageViewArray = [imageView0,imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9,imageView10,imageView11,imageView12,imageView13,imageView14]
+        generateNumber().isHidden = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func generateNumber() -> UIImageView {
+        return ImageViewArray[Int.random(in: 0..<ImageViewArray.count - 1)]
     }
-    */
-
+    
 }
