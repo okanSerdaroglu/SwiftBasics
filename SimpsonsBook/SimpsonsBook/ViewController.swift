231474dc37a207538fcb36b97498dc446d5d73c7
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-15 14:39:09 +0300

    initial commit

---
+++ SimpsonsBook/SimpsonsBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonsBook
+//
+//  Created by Okan Serdaroğlu on 15.04.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
