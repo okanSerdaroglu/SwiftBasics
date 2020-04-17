14e952f11a4e8b90df2bcea400baa04ccd4fcb5f
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-17 12:07:39 +0300

    views added

---
+++ SimpsonsBook/SimpsonsBook/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  SimpsonsBook
+//
+//  Created by Okan Serdaroğlu on 17.04.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var labelJob: UILabel!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var imageViewSimson: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
