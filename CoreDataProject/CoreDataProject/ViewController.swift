9ff9667be4afc5105bf4a0d37b121aeaff943d72
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-17 20:20:35 +0300

    getData from core data

--- CoreDataProject/CoreDataProject/ViewController.swift
+++ CoreDataProject/CoreDataProject/ViewController.swift
@@ -7,21 +7,66 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
     @IBOutlet weak var tableViewImageList: UITableView!
+    var nameArray = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableViewImageList.delegate = self
+        tableViewImageList.dataSource = self
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target:self, action: #selector(addPainting))
+        
+        getData()
+    }
+    
+    func getData() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest =  NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false // use cache data false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject] { // NSManagedObject is a core data object. If you dont cast it this type it is any.
+                if let name = result.value(forKey: "name") as? String {
+                    self.nameArray.append(name)
+                }
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+                
+            }
+        } catch {
+            print("error")
+        }
+        
     }
 
     @objc func addPainting(){
         performSegue(withIdentifier: "toDetailVC", sender: nil)
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+
 
 }
 
