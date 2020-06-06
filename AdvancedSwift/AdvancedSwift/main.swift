cf61b5a580ce30194875902ba8c59ba2307ba50c
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-06 23:29:54 +0300

    Tuple

--- AdvancedSwift/AdvancedSwift/main.swift
+++ AdvancedSwift/AdvancedSwift/main.swift
@@ -12,7 +12,7 @@ let classJames = MusicianClass(nameInput: "James",
                                ageInput: 50,
                                instrumentInput: "Guitar")
 
-print(classJames.age)
+//print(classJames.age)
 
 let structJames = MusicianStrcut(name: "James",
                                  age: 50,
@@ -53,8 +53,30 @@ print(structJames.age)*/
 
 // Function vs Mutating Function
 
-print(classJames.age)
+/*print(classJames.age)
 classJames.happyBirthday()
 
 print(structJames.age)
-print(structJames.age)
+print(structJames.age)*/
+
+let myTuple = (1,3)
+print(myTuple.0)
+
+let myTuple2 = (1,3,5)
+print(myTuple2.1)
+
+let myTuple3 = ("Okan",32)
+
+let myTuple4 = (10,[10,20,30])
+
+print(myTuple4.1[2])
+
+let predefinedTuple :(String,String)
+predefinedTuple.0 = "Okan"
+predefinedTuple.1 = "Serdaroglu"
+
+print(predefinedTuple)
+
+let newTuple = (name:"James",metallica:true)
+print(newTuple.metallica)
+print(newTuple.name)
