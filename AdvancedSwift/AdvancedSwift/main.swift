556ea3d41e8da48a6832246eeb3e57cc5f0b7d4e
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-06 22:40:34 +0300

    initial commit

---
+++ AdvancedSwift/AdvancedSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvancedSwift
+//
+//  Created by Okan Serdaroğlu on 6.06.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
