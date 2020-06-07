e76231e5317b3743c4e711ab4a5aaa39998b210e
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-07 23:10:49 +0300

    initial commit for swift UI

---
+++ FirstSwiftUIApp/FirstSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Okan Serdaroğlu on 7.06.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Swift UI!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
